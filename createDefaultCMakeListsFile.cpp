518f68a3906ef534d705b725515a393a813a03c3
onome.oyibo <onome09@hotmail.com>
2018-09-09 22:39:01 +0100

    chore: fixed main file

--- createDefaultCMakeListsFile.cpp
+++ createDefaultCMakeListsFile.cpp
@@ -1,20 +1,32 @@
 #include <fstream>
+#include <iostream>
+#include <string>
+#include <boost/filesystem.hpp>
 
-using boost::filesystem;
+using namespace boost::filesystem;
 
 int main(){
-  ofstream myfile;
+   std::string directoryName;
+
+  path p = boost::filesystem::current_path();
+  std::cout << p.filename() << std::endl;
+  std::string line = "add_executable(" + p.filename().string() + " ${SOURCES})";
+  std::ofstream myfile;
+  if ( boost::filesystem::exists( "CMakeLists.txt" ) ){
+    return 0;
+  }
   myfile.open ("CMakeLists.txt");
 
   myfile << "cmake_minimum_required (VERSION 3.0)\n";
-  myfile << "project()\n\n";
-    
+  myfile << "project(" << p.filename().string() << ")\n\n";
+
+  myfile << "file(GLOB SOURCES \"*.cpp\")\n";
+
+  myfile << "set(CMAKE_EXPORT_COMPILE_COMMANDS ON)\n";
+  myfile << line;
+
+  myfile.close();
+
   return 0;
 }
 
-cmake_minimum_required (VERSION 2.6)
-project (insertionSort)
-
-set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
-
-add_executable(insertionSort insertionSort.cpp)
