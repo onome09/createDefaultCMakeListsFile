19301d367b68fb4d826ff76bf336f3bb62de2ee6
onome.oyibo <onome09@hotmail.com>
2018-12-20 09:21:53 +0000

    add creating directories

--- createDefaultCMakeListsFile.cpp
+++ createDefaultCMakeListsFile.cpp
@@ -7,7 +7,7 @@ using namespace boost::filesystem;
 
 int main(){
    std::string directoryName;
-
+  system( "mkdir build && mkdir src && mkdir include && touch CMakeLists.txt" ) ;
   path p = boost::filesystem::current_path();
   std::cout << p.filename() << std::endl;
   std::string line = "add_executable(" + p.filename().string() + " ${SOURCES})";
@@ -20,7 +20,7 @@ int main(){
   myfile << "cmake_minimum_required (VERSION 3.0)\n";
   myfile << "project(" << p.filename().string() << ")\n\n";
 
-  myfile << "file(GLOB SOURCES \"*.cpp\")\n";
+  myfile << "file(GLOB SOURCES \"src/*.cpp\")\n";
 
   myfile << "set(CMAKE_EXPORT_COMPILE_COMMANDS ON)\n";
   myfile << line;
